e87bcaff43d453683dc694d9ec4c0302c040c977
Metehan <metehan.kara95@gmail.com>
2020-02-29 13:25:59 +0300

    React Native - The Best Books

---
+++ screens/Clubs.js
@@ -0,0 +1,16 @@
+import React from 'react' ;
+import {View, Text, StyleSheet } from 'react-native';
+
+const Clubs = props => {
+    return(
+        <View>
+            <Text>This is CLubs Screen !</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default Clubs;
---
+++ screens/Settings.js
@@ -0,0 +1,16 @@
+import React from 'react' ;
+import {View, Text, StyleSheet } from 'react-native';
+
+const Settings = props => {
+    return(
+        <View>
+            <Text>This is Settings Screen !</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default Settings;
